5f9bc42f4fbf670849a717598e7a8e73db8277dd
yeabsera <yeabseradeveloper@gmail.com>
2025-06-19 23:36:14 +0300

    feat(urls): integrate drf-yasg for API documentation and update API versioning
    - Replaced Spectacular with drf-yasg for enhanced API documentation capabilities.
    - Added schema view configuration with OpenAPI specifications.
    - Updated API URL patterns to include versioning and token refresh endpoint.
    - Introduced Swagger and ReDoc UI for API documentation access.

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,38 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenRefreshView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django API",
+        default_version="v1",
+        description="The backend API for Django",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="yeabseradeveloper@gmail.com"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+api_v1_patterns = [
+    path("", include(("core.api.urls", "api"), namespace="api")),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
 
 urlpatterns = [
-    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
     path("admin/", admin.site.urls),
-    path("api/", include(("core.api.urls", "api"), namespace="api")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/", include(api_v1_patterns)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from config.settings.debug_toolbar.setup import DebugToolbarSetup  # noqa
